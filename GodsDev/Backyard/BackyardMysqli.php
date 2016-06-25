18a8f84c8a8a3314b87d0f73ed1978fdb523fd48
GodsDev <rejthar@gods.cz>
2016-06-25 16:19:27 +0200

    object version comprises everything from the procedural version

--- GodsDev/Backyard/BackyardMysqli.php
+++ GodsDev/Backyard/BackyardMysqli.php
@@ -1,98 +1,12 @@
 <?php
 namespace GodsDev\Backyard;
 //@todo SHOULDN'T IT BE GodsDev\Backyard\Json ?
+//@TODO create TestBackyardMysqli.php
 
-// @todo - check that BackyardMysqli implements logic from procedural functions below
-///* * ****************************************************************************
-// * Database (MySQL) FUNCTIONS
-// */
-//
-///**
-// * mysql_query() with error message management and link identifier
-// * Replaces make_mysql_query($mysql_query_string, $ERROR_LOG_OUTPUT = true)
-// * @param string $mysql_query_string
-// * @param resource $link_identifier [optional]
-// * @param boolean $ERROR_LOG_OUTPUT [optional]
-// * @return a resource on success, or <b>FALSE</b> on error
-// */
-//function mysqlQuery($mysql_query_string, $link_identifier = NULL, $ERROR_LOG_OUTPUT = true) {
-//    //111010 - function is called even before error_log is initialized, therefore it is necessary to mute my_error_log, hence call make_mysql_query($sql,false);
-//    if ($ERROR_LOG_OUTPUT) {
-//        my_error_log("Start of query", 6, 11);
-//    }
-//    if (!$mysql_query_string) {
-//        if ($ERROR_LOG_OUTPUT) {
-//            my_error_log("mysql_query_string is empty", 1, 11); //debug               
-//        }
-//        return false;
-//    }
-//    if ($link_identifier == NULL) {
-//        $mysql_query_result = mysql_query($mysql_query_string);
-//    } else {
-//        $mysql_query_result = mysql_query($mysql_query_string, $link_identifier);
-//    }
-//    if (!$mysql_query_result) {
-//        my_error_log(mysql_errno() . ": " . mysql_error() . " /with query: $mysql_query_string", 1, 11);
-//    } elseif ($ERROR_LOG_OUTPUT) {
-//        my_error_log("$mysql_query_string", 5, 11); //debug
-//    }
-//    return $mysql_query_result;
-//}
-//
-///**
-// * Make a MySQL query and if the result is non empty, transforms the query result into a one or two dimensional array.
-// * Replaces customMySQLQuery()
-// * @param string $query
-// * @param bool $justOneRow [optional]
-// * @param resource $link_identifier [optional]
-// * @return mixed: array one or two dimensional or false
-// */
-//function mysqlQueryArray($query, $justOneRow = false, $link_identifier = NULL) {
-//    $mysql_query_result = backyard_mysql_query($query, $link_identifier) or backyard_dieGraciously('E100', "{$query} " . mysql_error()); // End script with a specific error message if mysql query fails
-//    if (is_bool($mysql_query_result)) {
-//        return $mysql_query_result; //For other type of SQL statements, INSERT, UPDATE, DELETE, DROP, etc, mysql_query() returns TRUE on success or FALSE on error.
-//    }
-//    if (!mysql_num_rows($mysql_query_result)) {//either false or zero
-//        my_error_log("Query {$query} returned no results", 5, 16);
-//        return false;
-//    }
-//    $result = array();
-//    while ($one_row = mysql_fetch_array($mysql_query_result, MYSQL_ASSOC)) {
-//        if ($justOneRow) {
-//            if (mysql_num_rows($mysql_query_result) > 1) {
-//                my_error_log("query={$query} returned more than one row, but only one row is requested", 11, 3);
-//            }
-//            return $one_row; //returns one dimensional array
-//        }
-//        $result[] = $one_row;
-//    }
-//    return $result; //returns two dimensional array
-//}
-//
-///**
-// * Find the next available id within selected dimension.
-// * It may be conditioned by an integer value of another dimension
-// * (Replacement for int function findFirstAvailableIdInRelevantTable($table, $ownerId, $relevantMetric))
-// * 
-// * @param resource $link_identifier
-// * @param string $table
-// * @param string $metricDimension
-// * @param string $primaryDimension [optional]
-// * @param int $primaryDimensionValue [optional]
-// * @return int
-// */
-//function mysqlNextIncrement($link_identifier, $table, $metricDimension, $primaryDimension = false, $primaryDimensionValue = false) {
-//    $result = 1; //default value
-//    $query = "SELECT `{$metricDimension}` FROM  `{$table}` "
-//            . (($primaryDimension && $metricDimension != $primaryDimension) ? ("WHERE  `{$primaryDimension}` =" . (int) $primaryDimensionValue . " ") : (""))
-//            . " ORDER BY `{$metricDimension}` DESC LIMIT 0 , 1;";
-//    $mysql_query_array = backyard_mysqlQueryArray($query, true, $link_identifier);
-//    if ($mysql_query_array) {
-//        $result += (int) $mysql_query_array[$metricDimension];
-//    }
-//    return $result;
-//}
-//
+/* * ****************************************************************************
+ * Database (MySQL) FUNCTIONS
+ */
+
 
 /**
  * __construct
@@ -162,14 +76,15 @@ class BackyardMysqli extends \mysqli {
      * if everything is OK, return the mysqli_result object
      * that is returned from parent query method
      * @param string $sql SQL to execute
-     * @return mysqli_result Object
+     * @return mysqli_result Object|false
      * @throws DBQueryException
      */
     public function query($sql, $ERROR_LOG_OUTPUT = true) { //S.R. upravuji dle functions.php ... make_mysql_query
+        //111010 - function is called even before error_log is initialized, therefore it is necessary to mute my_error_log, hence call make_mysql_query($sql,false); 160625 - is it still necessary?
         if ($ERROR_LOG_OUTPUT) {
             my_error_log("Start of query {$sql}", 5, 11);
         }
-        if ($sql == "") {
+        if (empty($sql) || !is_string($sql)) {
             if ($ERROR_LOG_OUTPUT) {
                 my_error_log("No mysql_query_string set. End of query", 1, 11); //debug
                 //my_error_log("End of query", 6, 11);                
@@ -183,7 +98,8 @@ class BackyardMysqli extends \mysqli {
         //if ($ERROR_LOG_OUTPUT) {
         //    my_error_log($sql, 5, 11);
         //}
-        $result = @parent::query($sql); //parent query method called with @ operator, to supress error messages
+        $result = //@
+                parent::query($sql); //parent query method called with @ operator, to supress error messages
         if ($this->errno != 0) {
             if ($ERROR_LOG_OUTPUT) {
                 my_error_log("{$this->errno} : {$this->error} /with query: {$sql}", 1, 11);
@@ -191,13 +107,14 @@ class BackyardMysqli extends \mysqli {
         }
         //}
         if ($ERROR_LOG_OUTPUT) {
-            my_error_log("End of query", 6, 11);
+            my_error_log("End of query {$sql}", 6, 11);
         }
         return $result;
     }
 
     /**
-     * 
+     * Make a MySQL query and if the result is non empty, transforms the query result into a one or two dimensional array.
+     * temporary note: Replaces customMySQLQuery()
      * @param string $sql
      * @param boolean $justOneRow (default = false)
      * @return 
@@ -240,7 +157,7 @@ class BackyardMysqli extends \mysqli {
      */
     public function nextIncrement($table, $metricDimension, $primaryDimension = false, $primaryDimensionValue = false) {
         $result = 1; //default value
-        $query = "SELECT `{$metricDimension}` FROM  `{$table}` "
+        $query = "SELECT `{$metricDimension}` FROM `{$table}` "
                 . (($primaryDimension && $metricDimension != $primaryDimension) ? ("WHERE  `{$primaryDimension}` =" . (int) $primaryDimensionValue . " ") : (""))
                 . " ORDER BY `{$metricDimension}` DESC LIMIT 0 , 1;";
         $mysql_query_array = $this->queryArray($query, true);
