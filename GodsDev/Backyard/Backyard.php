4d3487170b61a2427b199443c312516d29e0f6a3
GodsDev <rejthar@gods.cz>
2016-09-16 22:07:49 +0200

    fix: IDE hint by @var PHPdoc instead of @return for variable or getter /which requires ()/

--- GodsDev/Backyard/Backyard.php
+++ GodsDev/Backyard/Backyard.php
@@ -4,16 +4,59 @@ namespace GodsDev\Backyard;
 
 class Backyard {
 
-
+    /**
+     *
+     * @var array
+     */
     protected $BackyardConf = array();
-    public $BackyardArray; //object
-    public $Crypt; //object
-    public $BackyardError; //object
-    public $Geo; //object
-    protected $varHttp; //object
-    public $Json; //object
-    public $BackyardTime; //object
-    public $PageTimestamp; //object
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardArray BackyardArray object
+     */
+    public $BackyardArray;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardCrypt BackyardCrypt object
+     */
+    public $Crypt;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardError BackyardError object
+     */
+    public $BackyardError;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardGeo BackyardGeo object
+     */
+    public $Geo;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardHttp BackyardHttp object
+     */
+    public $Http;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardJson BackyardJson object
+     */
+    public $Json;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardTime BackyardTime object
+     */
+    public $BackyardTime;
+
+    /**
+     *
+     * @var float
+     */
+    public $PageTimestamp;
 
     /**
      * 
@@ -22,20 +65,13 @@ class Backyard {
     public function __construct(array $backyardConfConstruct = array()) {
         $this->BackyardTime = new BackyardTime();
         $this->PageTimestamp = $this->BackyardTime->getPageTimestamp(); //Initiation of $page_timestamp SHOULD be the first thing a page will do.
-
         $this->BackyardConf = $backyardConfConstruct;
         $this->BackyardError = new BackyardError($this->BackyardConf, $this->BackyardTime);
-
-        /*
-         * Works for functions. Btw: getter cannot be overwritten by mistake
-         * 
-         * @return \GodsDev\Backyard\BackyardArray BackyardArray object
-         */
-                $this->BackyardArray = new BackyardArray($this->BackyardError);
-                $this->Crypt = new BackyardCrypt($this->BackyardError);
-                $this->Geo = new BackyardGeo($this->BackyardError, $this->BackyardConf);
-                $this->varHttp = $this->Http();//new BackyardHttp($this->BackyardError);
-                $this->Json = new BackyardJson($this->BackyardError, $this->Http);
+        $this->BackyardArray = new BackyardArray($this->BackyardError);
+        $this->Crypt = new BackyardCrypt($this->BackyardError);
+        $this->Geo = new BackyardGeo($this->BackyardError, $this->BackyardConf);
+        $this->Http = new BackyardHttp($this->BackyardError);
+        $this->Json = new BackyardJson($this->BackyardError, $this->Http);
     }
 
     /**
@@ -49,16 +85,5 @@ class Backyard {
     public function newMysqli($host_port, $user, $pass, $db) {
         return new BackyardMysqli($host_port, $user, $pass, $db, $this->BackyardError);
     }
-    
-    /**
-     * 
-     * @return \GodsDev\Backyard\BackyardHttp BackyardHttp object
-     */
-    public function Http() {
-        return $this->varHttp ? : 
-                $this->varHttp = 
-                new BackyardHttp($this->BackyardError);
-    }
-
 
 }
