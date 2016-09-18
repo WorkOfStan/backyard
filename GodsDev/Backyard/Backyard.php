7df68cc18b02a7f7b1b2751b88cae3ba63e37863
GodsDev <rejthar@gods.cz>
2016-09-18 21:56:49 +0200

    IDE hint (#11)
    IDE hint on Classes instantiated in variables within Class Backyard by @var PHPdoc (instead of @return for variable or getter /which would require using () in the access instance member chain/

--- GodsDev/Backyard/Backyard.php
+++ GodsDev/Backyard/Backyard.php
@@ -4,9 +4,58 @@ namespace GodsDev\Backyard;
 
 class Backyard {
 
-
+    /**
+     *
+     * @var array
+     */
     protected $BackyardConf = array();
-    public $BackyardArray, $Crypt, $BackyardError, $Geo, $Http, $Json, $BackyardTime; //object
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardArray BackyardArray object
+     */
+    public $BackyardArray;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardCrypt BackyardCrypt object
+     */
+    public $Crypt;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardError BackyardError object
+     */
+    public $BackyardError;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardGeo BackyardGeo object
+     */
+    public $Geo;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardHttp BackyardHttp object
+     */
+    public $Http;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardJson BackyardJson object
+     */
+    public $Json;
+
+    /**
+     *
+     * @var \GodsDev\Backyard\BackyardTime BackyardTime object
+     */
+    public $BackyardTime;
+
+    /**
+     *
+     * @var float
+     */
     public $PageTimestamp;
 
     /**
@@ -16,15 +65,13 @@ class Backyard {
     public function __construct(array $backyardConfConstruct = array()) {
         $this->BackyardTime = new BackyardTime();
         $this->PageTimestamp = $this->BackyardTime->getPageTimestamp(); //Initiation of $page_timestamp SHOULD be the first thing a page will do.
-
         $this->BackyardConf = $backyardConfConstruct;
         $this->BackyardError = new BackyardError($this->BackyardConf, $this->BackyardTime);
-
-                $this->BackyardArray = new BackyardArray($this->BackyardError);
-                $this->Crypt = new BackyardCrypt($this->BackyardError);
-                $this->Geo = new BackyardGeo($this->BackyardError, $this->BackyardConf);
-                $this->Http = new BackyardHttp($this->BackyardError);
-                $this->Json = new BackyardJson($this->BackyardError, $this->Http);
+        $this->BackyardArray = new BackyardArray($this->BackyardError);
+        $this->Crypt = new BackyardCrypt($this->BackyardError);
+        $this->Geo = new BackyardGeo($this->BackyardError, $this->BackyardConf);
+        $this->Http = new BackyardHttp($this->BackyardError);
+        $this->Json = new BackyardJson($this->BackyardError, $this->Http);
     }
 
     /**
