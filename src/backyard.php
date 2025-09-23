a6a4dde6c794252576b37a7dd0118fbe547b5fe5
WorkOfStan <rejthar@stanislavrejthar.com>
2025-09-23 15:29:22 +0200

    chore: dead code from backyard 1 and backyard 2 removed
    ### Removed
    
    - dead code from backyard 1 and backyard 2 removed

--- src/backyard.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-error_log('Obsolete LIB 2 initiated. Exiting.');
-die('LIB2');
--- src/timezone.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-//http://stackoverflow.com/questions/3498128/guessing-users-timezone-in-php
-session_start();
-$_SESSION['time'] = $_GET['time'];
